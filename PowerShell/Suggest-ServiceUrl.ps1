84efc0730d825d649fd1f67d8f2dba42356534da
Johannes Wikman <jwikman@users.noreply.github.com>
2024-05-29 17:58:27 +0200

    Detect http protocol

--- PowerShell/Suggest-ServiceUrl.ps1
+++ PowerShell/Suggest-ServiceUrl.ps1
@@ -8,6 +8,7 @@ function Suggest-ServiceUrl {
 
     $ContainerName = Get-ContainerName -LaunchConfig $LaunchConfig
     $ServerInstance = Get-ValueFromLaunchJson -KeyName 'serverInstance' -LaunchConfig $LaunchConfig
+    $Server = Get-ValueFromLaunchJson -KeyName 'server' -LaunchConfig $LaunchConfig
     $ODataPort = 7048
     $CompanyName = Get-ValueFromALTestRunnerConfig -KeyName 'companyName'
     $SOAPPort = 7047
@@ -15,12 +16,14 @@ function Suggest-ServiceUrl {
     if ([String]::IsNullOrEmpty($CompanyName)) {
         $CompanyName = Select-BCCompany -ContainerName $ContainerName
     }
+    
+    $protocol = $Server.StartsWith('https') ? 'https' : 'http'
 
     if ($UseSOAP.IsPresent) {
-        return "http://$($ContainerName):$SOAPPort/$ServerInstance/WS/$CompanyName/Codeunit/TestRunner?tenant=default"
+        return "${protocol}://$($ContainerName):$SOAPPort/$ServerInstance/WS/$CompanyName/Codeunit/TestRunner?tenant=default"
     }
     else {
-        return "http://$($ContainerName):$ODataPort/$ServerInstance/ODataV4/TestRunner?company=$CompanyName&tenant=default"
+        return "${protocol}://$($ContainerName):$ODataPort/$ServerInstance/ODataV4/TestRunner?company=$CompanyName&tenant=default"
     }
 }
 
